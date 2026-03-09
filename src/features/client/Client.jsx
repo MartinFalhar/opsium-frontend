64f440dccda787f63922d9a807e9bb6a341ba47a
Martin Falhar <martin.falhar@gmail.com>
2026-03-09 20:48:30 +0100

    ai

--- src/features/client/Client.jsx
+++ src/features/client/Client.jsx
@@ -286,6 +286,7 @@ function Client() {
       <div className="left-container">
         {/* {console.log(`XXXCLIENT ${JSON.stringify(client)}`)} */}
         {Component ? <Component client={client} /> : null}
+        
       </div>
     </div>
   );
